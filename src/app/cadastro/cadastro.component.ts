b840c5f370d186980dd54ed26a54320cb2186e26
eymatsuda2016 <eymatsuda2016@outlook.com>
2025-05-06 23:17:31 -0300

    Finalizando a Parte Visual do Formulário

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -4,13 +4,17 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-cadastro',
   imports: [ FlexLayoutModule, 
               MatCardModule, 
               FormsModule, 
               MatFormFieldModule, 
-              MatInputModule ],
+              MatInputModule,
+              MatIconModule,
+              MatButtonModule ],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
