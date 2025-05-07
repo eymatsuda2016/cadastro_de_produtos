d6a7bb2d88009e7ce195b4286f91579be3460582
eymatsuda2016 <eymatsuda2016@outlook.com>
2025-05-07 11:28:12 -0300

    Populando dados do cliente atraves do formulário

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -6,6 +6,8 @@ import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { Cliente } from './cliente';
+
 @Component({
   selector: 'app-cadastro',
   imports: [ FlexLayoutModule, 
@@ -20,4 +22,9 @@ import { MatButtonModule } from '@angular/material/button';
 })
 export class CadastroComponent {
 
+  cliente: Cliente = Cliente.newCliente();
+
+  salvar(){
+    console.log("Dados do Cliente: ", this.cliente);
+  }
 }
