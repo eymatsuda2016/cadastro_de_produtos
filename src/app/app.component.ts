4b084dc0d408a03c3de3ea696ca1922672d8712b
eymatsuda2016 <eymatsuda2016@outlook.com>
2025-05-09 18:11:06 -0300

    Criando o Formulario de Consulta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { MatToolbarModule }from '@angular/material/toolbar';
-import { MatIconModule  } from '@angular/material/icon'
+import { MatIconModule  } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
